5e26af18504a3e2206bfea02c3ec837024847830
Ryan Salik <rssalik14@gmail.com>
2023-03-02 21:36:28 -0500

    Initial Commit

---
+++ src/lib/components/waves.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { createNoise2D } from 'simplex-noise';
+	import { lerpColor } from '$lib/util';
+
+	const simplex = createNoise2D();
+
+	let canvas: HTMLCanvasElement;
+	let w = 1,
+		h = 1;
+
+	let iw = 1,
+		ih = 1;
+
+	export let startingHeight = 0.7;
+    export let color = '#ff7030';
+
+	const resScale = 2;
+
+	$: {
+		w = iw * resScale;
+		h = ih * resScale;
+	}
+
+	let t = 0;
+
+	onMount(() => {
+		draw();
+	});
+
+	function n(x: number, y: number) {
+		return simplex(x / 450, y / 450);
+	}
+
+	function draw() {
+		const ctx = canvas.getContext('2d');
+
+		if (!ctx) return;
+
+		ctx.clearRect(0, 0, w, h);
+
+		drawWave(ctx, lerpColor(color, '#000000', .35), startingHeight - 0.05, 100, 0.5, -100);
+		drawWave(ctx, color, startingHeight, 80);
+		drawWave(ctx, lerpColor(color, '#ffffff', .15), startingHeight + 0.05, 60, 2.5, 100);
+
+		setTimeout(() => {
+			t += 1;
+			draw();
+		}, 10);
+	}
+
+	function drawWave(
+		ctx: CanvasRenderingContext2D,
+		color: string,
+		height: number,
+		variation: number,
+		speed = 1,
+		seed = 0
+	) {
+		const inc = w / 200;
+
+		ctx.beginPath();
+		ctx.moveTo(0, h);
+
+		for (let x = 0; x <= w + 1; x += inc) {
+			const y = height * h - n(x + t * speed, seed + t / 2) * variation;
+
+			ctx.lineTo(x, y);
+		}
+
+		ctx.lineTo(w, h);
+		ctx.lineTo(0, h);
+
+		ctx.fillStyle = color;
+		ctx.fill();
+	}
+</script>
+
+<svelte:window bind:innerHeight={ih} bind:innerWidth={iw} />
+
+<canvas bind:this={canvas} width={w} height={h} />
+
+<style lang="scss">
+	canvas {
+		position: absolute;
+		top: 0;
+		left: 0;
+
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,123 @@
+<script>
+	import Waves from '$lib/components/waves.svelte';
+
+	import Saos from 'saos';
+
+	import { fade } from 'svelte/transition';
+
+	let show = false;
+	let hasScrolled = false;
+
+	setTimeout(() => {
+		show = true;
+	}, 200);
+</script>
+
+<svelte:window
+	on:scroll={() => {
+		hasScrolled = true;
+	}}
+/>
+
+{#if show}
+	<section class="home">
+		<div class="header">
+			<div class="title" in:fade={{ duration: 300 }}>Ryan Salik</div>
+			<div class="subtitle" in:fade={{ duration: 300, delay: 300 }}>Student</div>
+		</div>
+
+		{#if !hasScrolled}
+			<div class="scroll nib" out:fade={{ duration: 300 }}><i>Scroll Down</i></div>
+		{/if}
+
+		<Waves />
+	</section>
+
+	<section class="main">
+		<Saos animation="fade-in .7s linear both">
+			<div class="section-title nib">Contact</div>
+		</Saos>
+
+		<div class="links">
+			<Saos animation="fade-in .7s linear both">
+				Email: <a href="mailto:rssalik14@gmail.com" class="mono">rssalik14@gmail.com</a>
+			</Saos>
+
+			<Saos animation="fade-in .7s linear both">
+				GitHub: <a href="https://github.com/rsalik" class="mono">rsalik</a>
+			</Saos>
+		</div>
+
+		<Waves color="#1a1a1a" startingHeight={0.85} />
+	</section>
+{/if}
+
+<style lang="scss">
+	.home {
+		font-size: 3em;
+
+		position: relative;
+
+		align-items: center;
+		justify-content: center;
+
+		.title {
+			position: relative;
+
+			margin-top: -1.6em;
+		}
+
+		.subtitle {
+			font-size: 0.5em;
+
+			position: relative;
+
+			margin-top: -0.8em;
+
+			text-align: right;
+
+			@media screen and (max-width: $mobile) {
+				font-size: 0.6em;
+
+				margin-top: -0.5em;
+			}
+		}
+	}
+
+	.scroll {
+		font-size: 0.3em;
+
+		position: absolute;
+		z-index: 100;
+		right: 0;
+		bottom: 0.5em;
+		left: 0;
+
+		text-align: center;
+
+		color: $bkg;
+	}
+
+	.main {
+		color: $bkg;
+		background: linear-gradient(to bottom, mix($accent, #fff, 85%), $accent);
+
+		.links {
+			z-index: 100;
+			@media screen and (max-width: $mobile) {
+				font-size: 1.2em;
+
+				padding: 0 2em;
+			}
+		}
+	}
+
+	@keyframes -global-fade-in {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+</style>
